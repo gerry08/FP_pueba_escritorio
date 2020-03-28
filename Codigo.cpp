68962cae5b58f054177553c5a7e1e21250609b2e
Gerardo <60832154+gerry08@users.noreply.github.com>
2020-03-27 22:39:26 -0600

    Update Codigo.cpp

--- Codigo.cpp
+++ Codigo.cpp
@@ -1,20 +1,23 @@
 #include<stdio.h> 
 #include<stdlib.h> 
 #include<time.h> 
+
 //gerardo Carbajal Alvarez
 // Este programa asigna de manera aleatoria el descuento 
+
 int main() 
 { 
 	float importeCompra,descuento; 
 	int num, pctDescuento; 
 	srand(time(NULL)); //Libreria time.h y stdlib.h
-	num=rand() % 8; // rango de valores // realiza la asignacion de la variable numero a una variable aleatoria
+	num=rand() % 8; // rango de valores y realiza la asignacion de la variable num a una variable aleatoria
 	printf ("\n----------------------------------------------------"); 
 	switch(num)  //menu de seleccion
 	{
 		
 		// me entrega el Descuento de manera aleaotria al azar 
-		// es deir me indica si obtuve bola negra roja, verde, o azul de manera aleatoria, al azar como si fueran dados
+		// es decir me indica si obtuve bola negra roja, verde, o azul de manera aleatoria, al azar como si fueran dados
+		// y de esa forma asigna lso descuentos	
 		case 0: 
 			printf ("\nBolita de color negro"); 
 			descuento=10; 
@@ -36,9 +39,9 @@ int main()
 	printf ("\nImporte total de la compra (sin descuento): $ "); 
 	scanf ("%f", &importeCompra); 	
 	printf ("\nSu importe total de compra fue: %.2f", importeCompra); 
-	descuento = (importeCompra - ( importeCompra* descuento / 100)); 
-	printf ("\nEl importe total con el descuento ser� de: $ %.2f \n\n", descuento); 
+	descuento = (importeCompra - ( importeCompra* descuento / 100)); // asigna el descuento
+	printf ("\nEl importe total con el descuento será¡ de: $ %.2f \n\n", descuento); 
 	getchar();
-	getchar();
+	getchar();// detiene la pantalla para que yo pueda ver mi descuento
 return 1; 
 } 
